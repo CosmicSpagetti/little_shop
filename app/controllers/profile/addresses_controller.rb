7d227ef2acf0fbe9920f9370aadda1fccc173f63
billy urrutia <billyurrutia@gmail.com>
2019-06-04 16:20:10 -0600

    Crud methods for addresses

--- app/controllers/profile/addresses_controller.rb
+++ app/controllers/profile/addresses_controller.rb
@@ -16,8 +16,38 @@ class Profile::AddressesController < ApplicationController
      flash[:danger] = @user.errors.full_messages
      render :new
     end 
+  end
+
+  def edit 
+    @user = current_user
+    @address = @user.addresses.first
   end 
 
+  def update
+    @address = Address.find(params[:id])
+    @user = @address.user
+    @address.update(address_params)
+    if @address.save
+      flash[:success] = "YoU UpdAtEd iT "
+      redirect_to profile_path(@user)
+    else
+      flash.now[:danger] = @user.errors.full_messages
+      render :edit
+    end 
+  end
+
+  def destroy
+    address = Address.find(params[:id])
+    
+    if address.check_orders == false
+      address.orders.clear
+      address.destroy
+      redirect_to profile_path
+    else
+      flash[:danger] = "Address tied to packaged/shipped order"
+      redirect_to profile_path
+    end
+  end 
   private 
 
   def address_params
