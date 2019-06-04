1babc8cccecb6786e081c24a6e13a78e934851f1
billy urrutia <billyurrutia@gmail.com>
2019-06-03 18:42:47 -0600

    address relationship test

--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -1,6 +1,6 @@
-require 'rails-helper'
+require 'rails_helper'
 
-RSpec.describe Address do 
+RSpec.describe Address, type: :model do 
   describe "validations" do 
     it { should validate_presence_of :address }
     it { should validate_presence_of :city }
@@ -8,6 +8,7 @@ RSpec.describe Address do
     it { should validate_presence_of :zip }
   end 
   describe 'relationships' do 
-    it { should belong_to :users }
+    it { should belong_to :user }
+    it { should have_many :orders }
   end 
 end 
